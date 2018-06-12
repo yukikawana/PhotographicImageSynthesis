8b1a9b8e1e647b1958e8b3f379bafad8a44d669d
yukikawana <kojirou.tensou@gmail.com>
2018-06-12 15:14:23 +0000

    vis add

--- vislog.py
+++ vislog.py
@@ -3,10 +3,13 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import sys
 
-ind=int( sys.argv[1])
+window = 1000
+ind=int(sys.argv[1])
+if(len(sys.argv)>2):
+    window=int(sys.argv[2])
 
 
 frame = test_df = pd.read_csv('log.csv', header=None, skiprows=16, sep=" ")
 print(frame.ix[0,0])
-frame.ix[:,ind].plot()
+frame.ix[:,ind].rolling(window=window).mean().plot()
 plt.show()
