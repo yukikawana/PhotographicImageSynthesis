089c8685296a02263a6c2704b6fcfec55bcb15d5
yukikawana <kojirou.tensou@gmail.com>
2018-06-13 16:22:28 +0000

    efficient feature extraction

--- 300x993_kitti_256.py
+++ 300x993_kitti_256.py
@@ -11,6 +11,7 @@ import dill
 sys.path.insert(0,"scd")
 from scd import get_tenors, process_image, imread_as_jpg 
 NUM_TRAINING_IMAGES = 7480
+#NUM_TRAINING_IMAGES = 30
 ckpt_filename = './ssd_300_kitti/ssd_model.ckpt'
 
 def lrelu(x):
@@ -22,7 +23,7 @@ def build_scd(input,sess,reuse=None):
         tf.get_variable_scope().reuse_variables()
     #end_points= get_tenors(ckpt_filename,sess, input=input, reuse=reuse)
     end_points= get_tenors(ckpt_filename,sess, input=input)
-    rs =r".*\/conv[0-9]\/conv[0-9]_[0-9]/Relu$"
+    rs =r"(.*\/conv[0-9]\/conv[0-9]_[0-9]/Relu$|.*ssd_300_vgg\/pool5\/MaxPool$)"
     rc = re.compile(rs)
     new_end_points = {}
     for op in tf.get_default_graph().as_graph_def().node:
@@ -32,14 +33,17 @@ def build_scd(input,sess,reuse=None):
     new_end_points["input"] = end_points["input"]
     return new_end_points
 
-def recursive_generator(label,sp):
+def recursive_generator(label,sp, reuse=None):
+    if reuse:
+        tf.get_variable_scope().reuse_variables()
+
     dim=512 if sp>=75 else 1024
     if sp==5:
         input=label
     else:
         spss2 = math.ceil(sp/2.)
         downsampled=tf.image.resize_area(label,(spss2,scaledic[spss2]),align_corners=False)
-        input=tf.concat([tf.image.resize_bilinear(recursive_generator(downsampled,spss2),(sp,scaledic[sp]),align_corners=True),label],3)
+        input=tf.concat([tf.image.resize_bilinear(recursive_generator(downsampled,spss2, reuse=reuse),(sp,scaledic[sp]),align_corners=True),label],3)
     net=slim.conv2d(input,dim,[3,3],rate=1,normalizer_fn=slim.layer_norm,activation_fn=lrelu,scope='g_'+str(sp)+'_conv1')
     net=slim.conv2d(net,dim,[3,3],rate=1,normalizer_fn=slim.layer_norm,activation_fn=lrelu,scope='g_'+str(sp)+'_conv2')
     if sp==150:
@@ -66,14 +70,19 @@ sess=tf.Session(config=config)
 is_training=True
 sp=150#spatial resolution: 256x512
 with tf.variable_scope(tf.get_variable_scope()):
-    label=tf.placeholder(tf.float32,[None,None,None,513])
-    label2 =tf.image.resize_bilinear(label,(150,496),align_corners=True)
-    generator=recursive_generator(label2,sp)
+    #label=tf.placeholder(tf.float32,[None,None,None,513])
+    #label2 =tf.image.resize_bilinear(label,(150,496),align_corners=False)
     real_image=tf.placeholder(tf.float32,[None,None,None,3])
-    fake_image=tf.placeholder(tf.float32,[None,None,None,3])
-
     vgg_real=build_scd(real_image,sess)
+
+    pool5 = vgg_real["pool5"]
+    label2_small = tf.concat((pool5,tf.expand_dims(1-tf.reduce_sum(pool5,axis=3),axis=3)),3)
+    label2 = tf.image.resize_bilinear(label2_small,(150,496),align_corners=True)
+
+    generator=recursive_generator(label2,sp)
+    fake_image=tf.placeholder(tf.float32,[None,None,None,3])
     vgg_fake=build_scd(generator,sess, reuse=True)
+
     train_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=tf.contrib.framework.get_name_scope())
     ssd_var_list=[v for v in train_vars if v.name.split("/")[0] == "ssd_300_vgg"]
     saver = tf.train.Saver(var_list=ssd_var_list)
@@ -97,8 +106,8 @@ sess.run(tf.initialize_variables(init_vars))
 
 if is_training:
     g_loss=np.zeros(NUM_TRAINING_IMAGES,dtype=float)
-    input_images=[None]*NUM_TRAINING_IMAGES
-    label_images=[None]*NUM_TRAINING_IMAGES
+    input_images=[None]*(NUM_TRAINING_IMAGES+100)
+    #label_images=[None]*NUM_TRAINING_IMAGES
     for epoch in range(1,101):
         if os.path.isdir("result_kitti256p_2/%04d"%epoch):
             continue
@@ -107,15 +116,14 @@ if is_training:
             st=time.time()
             cnt+=1
             if input_images[ind] is None:
-                label_images[ind]=np.load("hmpool5/%06d.npz"%ind)["arr_0"]#training label
+                #label_images[ind]=np.load("hmpool5/%06d.npz"%ind)["arr_0"]#training label
                
                 path =  '../kitti/training/image_2/%06d.png'%ind
                 img = imread_as_jpg(path)
                 img = cv2.resize(img, (496,150)) 
                 input_images[ind]=np.expand_dims(np.float32(img),axis=0)#training image
             #_,G_current,l0,l1,l2,l3,l4,l5=sess.run([G_opt,G_loss,p0,p1,p2,p3,p4,p5],feed_dict={label:np.concatenate((label_images[ind],np.expand_dims(1-np.sum(label_images[ind],axis=3),axis=3)),axis=3),real_image:input_images[ind],lr:min(1e-6*np.power(1.1,epoch-1),1e-4 if epoch>100 else 1e-3)})
-
-            _,G_current,l0,l1,l2,l3,l4,l5=sess.run([G_opt,G_loss,p0,p1,p2,p3,p4,p5],feed_dict={label:np.concatenate((label_images[ind],np.expand_dims(1-np.sum(label_images[ind],axis=3),axis=3)),axis=3),real_image:input_images[ind],lr:1e-4})#may try lr:min(1e-6*np.power(1.1,epoch-1),1e-4 if epoch>100 else 1e-3) in case lr:1e-4 is not good
+            _,G_current,l0,l1,l2,l3,l4,l5=sess.run([G_opt,G_loss,p0,p1,p2,p3,p4,p5],feed_dict={real_image:input_images[ind],lr:1e-4})#may try lr:min(1e-6*np.power(1.1,epoch-1),1e-4 if epoch>100 else 1e-3) in case lr:1e-4 is not good
             g_loss[ind]=G_current
             print("%d %d %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f"%(epoch,cnt,np.mean(g_loss[np.where(g_loss)]),np.mean(l0),np.mean(l1),np.mean(l2),np.mean(l3),np.mean(l4),np.mean(l5),time.time()-st))
         os.makedirs("result_kitti256p_2/%04d"%epoch)
@@ -123,14 +131,22 @@ if is_training:
         target.write("%f"%np.mean(g_loss[np.where(g_loss)]))
         target.close()
         saver.save(sess,"result_kitti256p_2/model.ckpt")
+
         if epoch%3==0:
             saver.save(sess,"result_kitti256p_2/%04d/model.ckpt"%epoch)
+
         for ind in range(NUM_TRAINING_IMAGES+1,NUM_TRAINING_IMAGES+51):
             path =  '../kitti/testing/image_2/%06d.png'%ind
+            #path =  '../kitti/training/image_2/%06d.png'%ind
             if not os.path.isfile(path):
                 continue
-            semantic = np.load("hmpool5/%06d.npz"%ind)["arr_0"]#training label
-            output=sess.run(generator,feed_dict={label:np.concatenate((semantic,np.expand_dims(1-np.sum(semantic,axis=3),axis=3)),axis=3)})
+            if input_images[ind] is None:
+                semantic = np.load("hmpool5/%06d.npz"%ind)["arr_0"]#training label
+                img = imread_as_jpg(path)
+                img = cv2.resize(img, (496,150)) 
+                input_images[ind]=np.expand_dims(np.float32(img),axis=0)#training image
+            output=sess.run(generator,feed_dict={label2_small:np.concatenate((semantic,np.expand_dims(1-np.sum(semantic,axis=3),axis=3)),axis=3)})
+            #output=sess.run(generator,feed_dict={real_image:input_images[ind]})
             output=np.minimum(np.maximum(output,0.0),255.0)
             """
             upper=np.concatenate((output[0,:,:,:],output[1,:,:,:],output[2,:,:,:]),axis=1)
